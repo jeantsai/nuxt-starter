96a34854bde2c5f35b2ef3e623525fac4d4d8579
jeantsai <jeantsai@qq.com>
2024-11-11 15:55:34 +0800

    Add GoogleFonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon']
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Doto: [100, 300, 400, 700],
+    },
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        fontFamily: {
+          sans: ["Doto", "sans-serif"],
+        },
+      },
+    },
+  },
+});
