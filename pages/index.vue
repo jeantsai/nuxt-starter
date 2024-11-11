42f1554a2d0543f60796407201a766cf11644aa8
jeantsai <jeantsai@qq.com>
2024-11-11 15:02:17 +0800

    Initial version with Tailwind and Iconify

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-blue-700 italic">
+      Hello, world! (Tailwind styled)
+    </h1>
+  </div>
+  <div class="flex items-center bg-cyan-200 m-2 p-2">
+    <p class="text-lg text-gray-700">
+      Icon from Iconify:
+    </p>
+    <Icon
+      name="ci:list-checklist"
+      size="32px"
+      style="color: black"
+    />
+  </div>
+</template>
