bde2a376699128280ba90978be9a936bd18112d2
jeantsai <jeantsai@qq.com>
2024-11-12 15:58:25 +0800

    Customized colors of Tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline text-blue-700 italic">
+    <h1 class="text-3xl font-bold underline text-primary italic">
       Hello, world! (Tailwind styled)
     </h1>
   </div>
   <div class="flex items-center bg-cyan-200 m-2 p-2">
-    <p class="text-lg text-gray-700">
-      Icon from Iconify:
-    </p>
-    <Icon
-      name="ci:list-checklist"
-      size="32px"
-      style="color: black"
-    />
+    <p class="text-lg text-gray-700">Icon from Iconify:</p>
+    <Icon name="ci:list-checklist" size="32px" style="color: black" />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require("tailwindcss/colors");
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.teal["500"],
+        secondary: colors.gray["500"],
+        success: colors.green["500"],
+        warning: colors.yellow["500"],
+        danger: colors.red["500"],
+        info: colors.blue["500"],
+      },
+    },
+  },
+};
